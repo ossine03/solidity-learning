c803982a8e1a095f8b879df2c5ab22ed0afee12e
오현아 <ossine03@gmail.com>
2025-10-03 15:53:01 +0900

    mytoken phase 1 - basic state (mapping, function, address, uint256, contract)

---
+++ contracts/MyToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.28;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimal;
+    }
+}
+    //function totalSupply() external view returns (uint256) {
+    //    return totalSupply;
+    //}
+    //function balanceOf(address owner) external view returns (uint256){
+    //    return balanceOf[owner];
+    //    
+    //}
+    
+    //function name() external view returns (string memory){
+    //    return name;
+    //}
