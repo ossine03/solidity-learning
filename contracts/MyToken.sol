dabc51a2a1984534c7f4c4fa7c38150a0a7dae64
오현아 <ossine03@gmail.com>
2025-10-07 15:30:50 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,27 +9,26 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimal, uint256 _amount
+        ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(1*10**uint256(decimals), msg.sender);
+        _mint(_amount *10**uint256(decimals), msg.sender);
     }
     function _mint(uint256 amount, address owner) internal{
         totalSupply += amount;
         balanceOf[owner] += amount;
 
     }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
 }
-    //function totalSupply() external view returns (uint256) {
-    //    return totalSupply;
-    //}
-    //function balanceOf(address owner) external view returns (uint256){
-    //    return balanceOf[owner];
-    //    
-    //}
     
-    //function name() external view returns (string memory){
-    //    return name;
-    //}
